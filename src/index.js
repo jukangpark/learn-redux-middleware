263ce862b788f6e4be4c97781b32e7df384629b3
jukangpark <skyxxx9339@naver.com>
2022-05-19 14:35:49 +0900

    redux-thunk 를 통해 만든 미들웨어를 통해 비동기 작업 수행

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import App from "./App";
+import rootReducer from "./modules";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
+import ReduxThunk from "redux-thunk";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(ReduxThunk, logger))
 );
+// 이런식으로 devTools 와 middleware 함께 사용가능.
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// 프로젝트에 리덕스를 적용 할 때는
+// src 디렉터리의 index.js 에서 root reducer 를 불러와서
+// 이를 통해 새로운 스토어를 만들고
+// provider 를 사용해서 프로젝트에 적용을 합니다.
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
+);
---
+++ src/modules/counter.js
@@ -0,0 +1,30 @@
+// 액션 타입
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+
+// 액션 생성 함수
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+
+// 초깃값 (상태가 객체가 아니라 그냥 숫자여도 상관 없습니다
+
+export const increaseAsync = () => (dispatch) => {
+  setTimeout(() => dispatch(increase()), 1000);
+}; //  이 함수들은 thunk 함수들이다.
+
+export const decreaseAsync = () => (dispatch) => {
+  setTimeout(() => dispatch(decrease()), 1000);
+};
+
+const initialState = 0;
+
+export default function counter(state = initialState, action) {
+  switch (action.type) {
+    case INCREASE:
+      return state + 1;
+    case DECREASE:
+      return state - 1;
+    default:
+      return state;
+  }
+}
